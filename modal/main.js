65a0563b11a7d399e98b8c50eaa313f719d25f75
Maria Kinsley <mariadfrancia2016@gmail.com>
2017-03-17 14:09:38 -0400

    modal-hw

---
+++ modal/main.js
@@ -0,0 +1,53 @@
+console.log("hello");
+// call elementby id
+var modal = document.getElementById("my-modal");
+// me creating funtion
+function myModal(){
+  // chosing element styles for chsnge on event
+  modal.style.display ="block";
+}
+// calling event listener
+modal.addEventListener("load",setTimeout(myModal, 3000))
+
+
+
+
+
+
+// setTimeout("alert"("hi"))
+
+
+
+
+
+
+
+
+
+
+
+
+// ===================================================
+// getElementById ("someId").addEventListener("click",funtion(event){
+//   alert('you clicked me')
+// })
+// changin the click to mouse enter or any other event will change
+// paragraph.addEventListener("click", funtion(){
+//   alert("you have clicked me")
+// })
+// setTimeout(function functionName() {
+// alert("hello")
+// },2000)
+// when DOM is ready
+// $(document).ready(function () {
+//
+//      // Attach Button click event listener
+//     $("#myBtn").click(function(){
+//
+//          // show Modal
+//          $('#myModal').modal('show');
+//     });
+// });
+// this will load the page
+// window.onload = function() {
+//   console.log("welcome")
